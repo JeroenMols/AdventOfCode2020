18d62fd938d14dfaa69365e65f2f6fb9897d5023
Jeroen Mols <jeroen@jeroenmols.com>
2020-12-02 14:10:28 +0100

    upgrade to node module

--- test/challenge_03.test.js
+++ test/challenge_03.test.js
@@ -1,19 +1,18 @@
+import {countOccurrences, insertUniformSeparator} from "../challenge_03.js";
+import * as assert from "assert";
 
-const assert = require('assert');
-const insertUniformSeparator = require("../challenge_03");
-const functions = require("../challenge_03");
 
 describe('Test data conversion', () => {
 
     it('insert uniform data separators', () => {
         let data = "1-3 a: abcde"
 
-        assert.equal(functions.insertUniformSeparator(data), "1|3|a|abcde");
+        assert.equal(insertUniformSeparator(data), "1|3|a|abcde");
     });
 
     it('count occurrence of letter', () => {
         let data = "aaab"
-        assert.equal(functions.countOccurrences(data, "a"), 3);
+        assert.equal(countOccurrences(data, "a"), 3);
     });
 
 });
